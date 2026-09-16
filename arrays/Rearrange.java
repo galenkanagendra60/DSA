6f7bac0fef03ca2baaa5cb6bf69384dc7026b54d
nagendra <galenkanagendra60@gmail.com>
2026-09-16 21:18:16 +0530

    17 - problem in arrays

--- arrays/Rearrange.java
+++ arrays/Rearrange.java
@@ -1,40 +1,64 @@
 import java.util.Scanner;
-class Rearrange{
-    public static int[] rearrange(int[] nums)
-    {
-        int[] result = new int[nums.length];
-        int pos =0;
-        int neg = 1;
-        for(int num :nums)
-        {
-            if(num>0)
-            {
-               result[pos]= num;
-               pos+=2;    
-            }
-            else if(num<0)
-            {
-                result[neg] = num;
-                neg+=2;
-            }
+
+class Rearrange {
+
+    public static void rearrange(int[] nums) {
+
+        int i = nums.length - 2;
+
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
         }
-        return result;        
+
+        if (i >= 0) {
+
+            int j = nums.length - 1;
+
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        int left = i + 1;
+        int right = nums.length - 1;
+
+        while (left < right) {
+
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
+        }
     }
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of an array");
+
+        System.out.print("Enter the size of an array: ");
         int n = sc.nextInt();
-        System.out.println("Enter the elements of an array :");
-        int[] nums= new int[n];
-        for(int i=0;i<n;i++)
-        {
-            nums[i]=sc.nextInt();
+
+        System.out.println("Enter the array elements:");
+
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
         }
-        int[] result = rearrange(nums);
-        
-        for(int j=0;j<result.length;j+=2)
-        {
-            System.out.println(result[j]+" "+result[j+1]);
+        rearrange(nums);
+
+        System.out.println("Next permutation:");
+
+        for (int num : nums) {
+            System.out.print(num + " ");
         }
+
+        sc.close();
     }
 }
