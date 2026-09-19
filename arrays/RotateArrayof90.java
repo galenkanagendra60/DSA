cb738730695da9445348c18757593c15d44a24b2
nagendra <galenkanagendra60@gmail.com>
2026-09-19 10:24:08 +0530

    20 - problem in arrays

---
+++ arrays/RotateArrayof90.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class RotateArrayof90 {
+    public static void rotate(int[][] matrix) {
+
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array :");
+        int n = sc.nextInt();
+        System.out.println("Enter the array elements :");
+        int[][] matrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+
+        }
+        rotate(matrix);
+        System.out.println("Rotated Array elements :");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+
+    }
+}
